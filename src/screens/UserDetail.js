181da368a1e919c1549c052c495417e701de30ea
AngelTrinidad <96trinidad@gmail.com>
2020-04-21 13:33:16 -0400

    add route param in UserDetail

--- src/screens/UserDetail.js
+++ src/screens/UserDetail.js
@@ -2,7 +2,12 @@ import React from 'react';
 import {Text} from 'react-native';
 import ViewContainer from '../components/ViewContainer';
 
-const UserDetail = ({navigation}) => {
+const UserDetail = ({navigation, route}) => {
+  /**
+   * User's id from navigation param
+   */
+  const {id} = route.params;
+
   return (
     <ViewContainer>
       <Text>Detalle del usuario</Text>
