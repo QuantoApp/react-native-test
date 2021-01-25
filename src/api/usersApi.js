9edefdb4de2f4b1f5f0e368e975fa2766edcebc1
fromero <cursos.online1988@gmail.com>
2021-01-25 16:42:09 -0300

    Agrega constante para api url

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { API_URL } from "../utils/constants";
 
 export function getUsers() {
-  return axios.get(`https://reqres.in/api/users`);
+  return axios.get(`${API_URL}/users`);
 }
 
 export function getUserDetail(userId) {
-  return axios.get(`https://reqres.in/api/users/${userId}`);
+  return axios.get(`${API_URL}/users/${userId}`);
 }
